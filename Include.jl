9f9f77ad4b58a92c54bb1a9391145fd6d1d4973b
kuang-2000 <143023280+kuang-2000@users.noreply.github.com>
2023-12-13 00:41:28 -0500

    Initial commit

---
+++ Include.jl
@@ -0,0 +1,42 @@
+# setup the paths -
+const _ROOT = pwd();
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+const _PATH_TO_DATA = joinpath(_ROOT, "data");
+
+# download external packages
+import Pkg; 
+Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git")
+Pkg.add(path="https://github.com/varnerlab/VLDecisionsPackage.jl.git")
+Pkg.activate("."); Pkg.instantiate(); Pkg.update()
+
+# load external packages -
+using VLDecisionsPackage
+using VLQuantitativeFinancePackage
+using CSV
+using DataFrames
+using ForwardDiff
+using PrettyTables
+using Plots
+using Colors
+using Statistics
+using LinearAlgebra
+using Optim
+using StatsPlots
+using Distributions
+using JLD2
+using FileIO
+using LinearAlgebra
+using MathOptInterface
+
+# load my color palette -
+colors = Dict{Int,RGB}();
+colors[1] = colorant"#0077BB"
+colors[2] = colorant"#88CCEE"
+colors[3] = colorant"#EE7733"
+colors[4] = colorant"#CC3311"
+
+# load my codes -
+include(joinpath(_PATH_TO_SRC, "Compute.jl"));
+include(joinpath(_PATH_TO_SRC, "Files.jl"));
+include(joinpath(_PATH_TO_SRC, "Budget.jl"));
+include(joinpath(_PATH_TO_SRC, "Indifference.jl"));
---
+++ README.md
@@ -0,0 +1,2 @@
+# PS3-CHEME-5760-OptionPriceCalculation-Fall-2023
+Template repository for Problem Set 3 (PS3) for CHEME 5760
